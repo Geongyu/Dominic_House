7814e81d0a254fd79db895289e40117decd4712e
SuhangGeongyu <37532168+SuhangGeongyu@users.noreply.github.com>
2018-05-10 15:30:17 +0900

    Add files via upload

--- 노드 js 스터디/app_file2.js
+++ 노드 js 스터디/app_file2.js
@@ -2,12 +2,34 @@ var express = require("express");
 var app = express(); // 익스프레스 추가
 var bodyParser = require('body-parser'); // 바디파서 인클루드
 var fs = require('fs'); // 파일시스템 제어 모듈
+var multer = require('multer'); // 파일 업로드를 위한 모듈
+var upload = multer({ storage: 'uploads/' })
+    // dest를 사용하지 않고 스토리지를 사용할때에는 추가 설정이 필요하다
+    // dest만세!!!!!!!!!!!!!!!!!!!!!!!
+
+var _storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+      },
+  filename: function (req, file, cb) {
+        cb(null, file.originalname);
+      }
+})
+    // 멀터 스토리지 사용하기 위하여 스토리지를 정의한다.
+
+var upload = multer({ storage: _storage })
 
 app.use(bodyParser.urlencoded({ extended:false})); // 바디파서 사용
 app.set('views', './views_file'); // 템플릿이 저장되어 있는 위치 지정
 app.set('view engine', 'jade'); // Jade 를 사용하기 위하여
 app.locals.pretty = true; // jade를 줄바꿈 해준다
-
+app.get('/upload', function (req, res) {
+    res.render('upload');
+});
+app.post('/upload', upload.single('userfile'), function(req, res){
+    console.log(req.file);
+    res.send('Uploaded : '+ req.file.filename);
+});
 app.get('/topic/new', function(req, res){
     fs.readdir('data', function (err, files)  // 파일 목록 불러오기 위하여
     {
