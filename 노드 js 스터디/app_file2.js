ca3724a1a602f77f598d463369f9e8c8595d761f
SuhangGeongyu <37532168+SuhangGeongyu@users.noreply.github.com>
2018-05-14 11:39:31 +0900

    Add files via upload

--- 노드 js 스터디/app_file2.js
+++ 노드 js 스터디/app_file2.js
@@ -1,4 +1,102 @@
-var express = require("express");
+/* var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var _storage = multer.diskStorage({
+      destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+      },
+  filename: function (req, file, cb) {
+        cb(null, file.originalname);
+      }
+})
+var upload = multer({ storage: _storage })
+    var fs = require('fs');
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+      host     : 'localhost',
+      user     : 'root',
+      password : '111111',
+      database : 'o2'
+});
+conn.connect();
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.locals.pretty = true;
+app.use('/user', express.static('uploads'));
+app.set('views', './views_mysql');
+app.set('view engine', 'jade');
+app.get('/upload', function(req, res){
+      res.render('upload');
+    });
+app.post('/upload', upload.single('userfile'), function(req, res){
+      res.send('Uploaded : '+req.file.filename);
+    });
+app.get('/topic/new', function(req, res){
+      fs.readdir('data', function(err, files){
+            if(err){
+                  console.log(err);
+                  res.status(500).send('Internal Server Error');
+                }
+            res.render('new', {topics:files});
+          });
+    });
+app.get(['/topic', '/topic/:id'], function(req, res){
+      var sql = 'SELECT id,title FROM topic';
+      conn.query(sql, function(err, topics, fields){
+            var id = req.params.id;
+            if(id){
+                  var sql = 'SELECT * FROM topic WHERE id=?';
+                  conn.query(sql, [id], function(err, topic, fields){
+                        if(err){
+                              console.log(err);
+                              res.status(500).send('Internal Server Error');
+                            } else {
+                              res.render('view', {topics:topics, topic:topic[0]});
+                            }
+                      });
+                } else {
+                  res.render('view', {topics:topics});
+                }
+          });
+    });
+app.post('/topic', function(req, res){
+      var title = req.body.title;
+      var description = req.body.description;
+      fs.writeFile('data/'+title, description, function(err){
+            if(err){
+                  console.log(err);
+                  res.status(500).send('Internal Server Error');
+                }
+            res.redirect('/topic/'+title);
+          });
+    })
+app.listen(3000, function(){
+      console.log('Connected, 3000 port!');
+    })
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ var express = require("express");
 var app = express(); // 익스프레스 추가
 var bodyParser = require('body-parser'); // 바디파서 인클루드
 var fs = require('fs'); // 파일시스템 제어 모듈
@@ -6,6 +104,15 @@ var multer = require('multer'); // 파일 업로드를 위한 모듈
 var upload = multer({ storage: 'uploads/' })
     // dest를 사용하지 않고 스토리지를 사용할때에는 추가 설정이 필요하다
     // dest만세!!!!!!!!!!!!!!!!!!!!!!!
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : '111111',
+    database : 'o2'
+});
+
+connection.connect();
 
 
 
@@ -22,7 +129,7 @@ var _storage = multer.diskStorage({
 var upload = multer({ storage: _storage })
 
 app.use(bodyParser.urlencoded({ extended:false})); // 바디파서 사용
-app.set('views', './views_file'); // 템플릿이 저장되어 있는 위치 지정
+app.set('views', './views_mysql'); // 템플릿이 저장되어 있는 위치 지정
 app.set('view engine', 'jade'); // Jade 를 사용하기 위하여
 app.locals.pretty = true; // jade를 줄바꿈 해준다
 app.get('/upload', function (req, res) {
@@ -44,29 +151,49 @@ app.get('/topic/new', function(req, res){
 });
 
 app.get(['/topic', '/topic/:id'], function (req, res) {
-    fs.readdir('data', function (err, files)  // 파일 목록 불러오기 위하여
-    {
-        if(err) {
-            console.log(err);
-            res.status(500).send('Internal Server Error');
-        }
+    var sql = 'SELECT id, title FROM topic';
+    conn.query(sql, function (err, topics, fields) {
         var id = req.params.id;
         if(id){
-            // id값이 있을때
-            fs.readFile('data/' + id, 'utf8', function (err, data) {
-                if (err) {
-                    console.log(err);
-                    res.status(500).send('Internal Server Error');
+            var sql = 'SELECT * FROM topic WHERE id=?';
+            conn.query(sql, [id], function(err, topic, fields){
+                if(err){
+                    consle.log(err);
+                    res.status(500).send('Internal Sever Error');
+                } else {
+                    res.render('view', {topics:topics, topic:topic[0]});
                 }
-                res.render('view', {topics: files, title: id, description:data});
-            })
+            });
         } else {
-            // id 값이 없을 때
-            res.render('view', {topics: files, title:'Welcome', description:'Hello Java Script for server.'});
-        }
-    })
-});
+            res.render('view', {topics:topics});
 
+        }
+        //  res.render('view', {topics: topics});
+    });
+});
+    /* fs.readdir('data', function (err, files)  // 파일 목록 불러오기 위하여
+     {
+         if(err) {
+             console.log(err);
+             res.status(500).send('Internal Server Error');
+         }
+         var id = req.params.id;
+         if(id){
+             // id값이 있을때
+             fs.readFile('data/' + id, 'utf8', function (err, data) {
+                 if (err) {
+                     console.log(err);
+                     res.status(500).send('Internal Server Error');
+                 }
+                 res.render('view', {topics: files, title: id, description:data});
+             })
+         } else {
+             // id 값이 없을 때
+             res.render('view', {topics: files, title:'Welcome', description:'Hello Java Script for server.'});
+         }
+     })
+ });
+*/
 /* app.get('/topic/:id', function (req,res) {
     var id = req.params.id; // 파일 자동으로 읽기
     fs.readdir('data', function (err, files)  // 파일 목록 불러오기 위하여
@@ -88,19 +215,19 @@ app.get(['/topic', '/topic/:id'], function (req, res) {
 
 */
 
-app.post('/topic', function (req,res) // 포스트를 가져온다
- {
-     var title = req.body.title;
-     var description = req.body.description;
-     fs.writeFile('data/'+title, description, function (err) {
-         if(err){
-             console.log(err);
-            res.status(500).send('Internal Server Error');
-         }
-         res.redirect('/topic/'+title);
-     });
+    app.post('/topic', function (req, res) // 포스트를 가져온다
+    {
+        var title = req.body.title;
+        var description = req.body.description;
+        fs.writeFile('data/' + title, description, function (err) {
+            if (err) {
+                console.log(err);
+                res.status(500).send('Internal Server Error');
+            }
+            res.redirect('/topic/' + title);
+        });
 
-})
-app.listen(3000, function () {
-    console.log('connected, 3000 port!');
-})
+    })
+    app.listen(3000, function () {
+        console.log('connected, 3000 port!');
+    })
--- 노드 js 스터디/database_mysql.js
+++ 노드 js 스터디/database_mysql.js
@@ -7,14 +7,39 @@ var connection = mysql.createConnection({
 });
 
 connection.connect();
-
+/* 셀렉트문
 var sql = 'SELECT * FROM topic'
 connection.query(sql, function (err, rows, fields) {
     if(err) {
         console.log(err);
     } else {
-        console.log('rows', rows);
-        console.log('fields', fields);
+        for(var i=0; i<rows.length; i++){
+            console.log(rows[i].title);
+        }
+    }
+});
+ */
+/*
+var sql = 'INSERT INTO topic (title, description, author) VALUES(?, ?, ?)';
+var params = ['Supervisor', 'Watcher', 'graphittie'];
+// 파람으로 데이터를 가져와서 밑에 추가 시켜줫음, SQL 인젝션을 막을 수 있음
+connection.query(sql, params, function (err, rows, fields) {
+    if(err){
+        console.log(err);
+    } else {
+        console.log(rows);
+    }
+});
+*/
+var sql = /* 'UPDATE topic SET title=?, author=? WHERE id=?'; */
+        'DELETE FROM topic WHERE id=?';
+var params = [1];
+// 파람으로 데이터를 가져와서 밑에 추가 시켜줫음, SQL 인젝션을 막을 수 있음
+connection.query(sql, params, function (err, rows, fields) {
+    if(err){
+        console.log(err);
+    } else {
+        console.log(rows);
     }
 });
 connection.end();
