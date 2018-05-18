745151619012add454329aaeba02709d71c0e552
SuhangGeongyu <37532168+SuhangGeongyu@users.noreply.github.com>
2018-05-18 17:45:48 +0900

    Add files via upload

--- 시작을 위한 한걸음/app.js
+++ 시작을 위한 한걸음/app.js
@@ -1,41 +1,32 @@
 var createError = require('http-errors');
 var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 var bodyParser = require('body-parser');
-var mysql = require('mysql');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '111111',
-    database: 'board'
-});
-
-connection.connect();
-
-// mysql 연동을 위한
 
 // view engine setup
 app.set('view engine', 'jade');
 app.set('views', './views');
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 app.use(bodyParser.urlencoded({ extended: false}));
 // catch 404 and forward to error handler
 
 app.get('/main', function (req, res) {
     res.render('main');
-});
+})
 app.get('/main/profile', function (req, res) {
     res.render('profile');
-});
+})
 app.get('/', function (req, res) {
-    res.render('main');
-});
-app.get('/main/deveolpe', function (req, res) {
-    res.render('develope');
-});
-app.get('/main/study', function (req, res) {
-    res.render('study');
-});
+    res.send("Return to Main Pages!")
+})
 
 module.exports = app;
 
