f1888455ad229bda5756c3e256de9c486090fe36
SuhangGeongyu <37532168+SuhangGeongyu@users.noreply.github.com>
2018-05-18 17:48:44 +0900

    Add files via upload

--- 시작을 위한 한걸음/app.js
+++ 시작을 위한 한걸음/app.js
@@ -1,32 +1,48 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 var bodyParser = require('body-parser');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var mysql = require('mysql');
+var fs = require('fs');
 var app = express();
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '111111',
+    database: 'board'
+});
+
+connection.connect();
+
+// mysql 연동을 위한
 
 // view engine setup
 app.set('view engine', 'jade');
 app.set('views', './views');
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use(bodyParser.urlencoded({ extended: false}));
 // catch 404 and forward to error handler
 
 app.get('/main', function (req, res) {
     res.render('main');
-})
+});
 app.get('/main/profile', function (req, res) {
     res.render('profile');
-})
+});
 app.get('/', function (req, res) {
-    res.send("Return to Main Pages!")
-})
+    res.render('main');
+});
+app.get('/main/deveolpe', function (req, res) {
+    res.render('develope');
+});
+app.get('/main/study', function (req, res) {
+    var sql = 'SELECT No, Title, Redate FROM board01'
+    connection.query(sql, function (err, boards, fields) {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }
+        res.render('study', {boards: boards});
+    })
+});
 
 module.exports = app;
 
