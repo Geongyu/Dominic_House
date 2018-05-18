6f90e326c04e422427a5dd176a0943f1bac4e0e8
SuhangGeongyu <37532168+SuhangGeongyu@users.noreply.github.com>
2018-05-18 11:18:59 +0900

    Add files via upload

--- 노드 js 스터디/app_session.js
+++ 노드 js 스터디/app_session.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var session = require('express-session');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
-    saveUninitialized:true
+    saveUninitialized: true
 }));
 app.get('/count', function (req, res) {
     if(req.session.count) {
@@ -16,6 +19,56 @@ app.get('/count', function (req, res) {
 
     res.send('count : '+req.session.count);
 });
+app.get('/auth/login', function (req, res) {
+    var output = `
+<h1>Login</h1>
+    <form action="/auth/login" method="post">
+    <p> 
+        <input type="text" name="username" placeholder="username" >
+    </p>
+    <p>
+        <input type="password" name="password" placeholder="password">
+    </p>
+    <p>
+    <input type="submit">
+</p>
+</formac>`;
+    res.send(output);
+});
+app.get('/welcome', function (req, res) {
+    if(req.session.displayName){
+        res.send(`
+        <h1> Hello, ${req.session.displayName} </h1>
+        <a href="/auth/logout">logout</a>
+        `);
+    } else {
+        req.send(`
+        <h1> Welcome </h1>
+        <a href="/auth/login">Login</a>
+        `);
+    }
+
+});
+app.get('/auth/logout', function (req, res) {
+   delete req.session.displayName;
+    res.redirect('/welcome');
+});
+app.post('/auth/login', function (req,res) {
+    var uname = req.body.username;
+    var pwd = req.body.password;
+    var user = {
+        username:'RRos',
+        password:'111',
+        displayName : 'Rostarine'
+    };
+    if(uname === user.username && pwd === user.password){
+        req.session.displayName = user.displayName;
+        res.redirect('/welcome');
+    } else {
+        res.send('Who are you <a href="/auth/login">login</a>');
+    }
+})
+
 app.get('/tmp', function (req, res) {
     res.send('result : '+req.session.count);
 });
