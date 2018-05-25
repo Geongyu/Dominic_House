3aa30237c6176295906b40d51baa90e32ff495f9
SuhangGeongyu <37532168+SuhangGeongyu@users.noreply.github.com>
2018-05-25 17:48:33 +0900

    Add files via upload

--- 시작을 위한 한걸음/app.js
+++ 시작을 위한 한걸음/app.js
@@ -97,6 +97,41 @@ app.post('/main/study', function(req, res, next) {
 */
 // Ajax를 이렇게 사용하는게 아닌가 보다..
 
+const users = [
+    {
+        user_id: 'root',
+        user_nickname: 'Root',
+        user_pwd: '123456'
+    },
+    {
+        user_id: 'mil',
+        user_nickname: '육군군사기밀',
+        user_pwd: '1q2w3e4r'
+    }
+]
+const findUser = (user_id, user_pwd) => {
+    // id와 password가 일치하는 유저 찾는 함수, 없으면 undefined 반환
+    return users.find( v => (v.user_id === user_id && v.user_pwd === user_pwd) );
+}
+const findUserIndex = (user_id, user_pwd) => {
+    // 일치하는 유저의 index값(유니크) 반환
+    return users.findIndex( v => (v.user_id === user_id && v.user_pwd === user_pwd) );
+}
+
+app.get(('/login'), function(req, res){
+    res.render('login')
+});
+app.post('/login', (req, res) => {
+    const body = req.body; // body-parser 사용
+    if( findUser( body.user_id, body.user_pwd ) ) {
+        // 해당유저가 존재한다면
+        req.session.user_uid = findUserIndex( body.user_id, body.user_pwd ); //유니크한 값 유저 색인 값 저장
+        res.redirect('/');
+    } else {
+        res.send('유효하지 않습니다.');
+    }
+});
+
 app.get(['/main/study', '/study/:No'], function (req, res) {
     var sql = 'SELECT No, Title, Content FROM board01';
     connection.query(sql, function (err, boards, fields) {
@@ -115,6 +150,10 @@ app.get(['/main/study', '/study/:No'], function (req, res) {
             res.render('study', {boards: boards});
         }
     });
+});
+app.get('/logout', (req, res) => {
+    delete req.session.user_uid;
+    res.redirect('/main');
 });
 
 app.get('/main/study/add', function(req, res) {
