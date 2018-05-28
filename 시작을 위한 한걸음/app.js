cdbad7ea7b67db717babfd69b2c140d7fb024367
SuhangGeongyu <37532168+SuhangGeongyu@users.noreply.github.com>
2018-05-28 18:05:27 +0900

    Add files via upload

--- 시작을 위한 한걸음/app.js
+++ 시작을 위한 한걸음/app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
-var fs = require('fs');
 var app = express();
 var session = require('express-session');
 
@@ -34,16 +33,11 @@ app.set('views', './views');
 app.use(bodyParser.urlencoded({ extended: false}));
 // catch 404 and forward to error handler
 
-
-
-app.get('/main', function (req, res) {
+app.get(['/main', '/'], function (req, res) {
     res.render('main');
 });
 app.get('/main/profile', function (req, res) {
     res.render('profile');
-});
-app.get('/', function (req, res) {
-    res.render('main');
 });
 app.get('/main/develope', function (req, res) {
     var sql = 'SELECT * FROM board02';
@@ -55,72 +49,26 @@ app.get('/main/develope', function (req, res) {
             res.render('develope', {boards: boards});
          })
 });
-/*
-app.get('/study/:No'), function(req, res) {
-    console.log('여기까지옴');
-    var sql = 'SELECT * FROM board01';
-    connection.query(sql, function (err, boards, fields) {
-        res.send(boards);
-    })
-        var No = req.params.No;
-        if (No) {
-            var sql = 'SELECT * FROM board01 WHERE No=?';
-            connection.query(sql, [No], function (err, board01, fields) {
-                if (err) {
-                    console.log(err);
-                    res.status(500).send('Internal Server Error');
-                } else {
-                    res.render('study', {boards: boards, board01: board01[0]});
-                }
-            });
-        } else {
-            res.render('study', {boards: boards});
-        }
-    });
-}; */
-/*
-app.post('/main/study', function(req, res, next) {
-    var sql = 'SELECT * FROM board01';
-    connection.query(sql, function (err, boards, fields) {
-        if (err) {
-            console.log(err);
-            res.status(500).send('서버 오류');
-        }
-        var msg = " " + req.body.Content;
-
-        res.send({
-            msg2: msg
-         })
-
-    })
-});
-*/
-// Ajax를 이렇게 사용하는게 아닌가 보다..
 
 const users = [
     {
         user_id: 'root',
-        user_nickname: 'Root',
         user_pwd: '123456'
-    },
-    {
-        user_id: 'mil',
-        user_nickname: '육군군사기밀',
-        user_pwd: '1q2w3e4r'
     }
-]
+];
 const findUser = (user_id, user_pwd) => {
     // id와 password가 일치하는 유저 찾는 함수, 없으면 undefined 반환
     return users.find( v => (v.user_id === user_id && v.user_pwd === user_pwd) );
-}
+};
 const findUserIndex = (user_id, user_pwd) => {
     // 일치하는 유저의 index값(유니크) 반환
     return users.findIndex( v => (v.user_id === user_id && v.user_pwd === user_pwd) );
-}
+};
 
 app.get(('/login'), function(req, res){
     res.render('login')
 });
+
 app.post('/login', (req, res) => {
     const body = req.body; // body-parser 사용
     if( findUser( body.user_id, body.user_pwd ) ) {
@@ -151,6 +99,7 @@ app.get(['/main/study', '/study/:No'], function (req, res) {
         }
     });
 });
+
 app.get('/logout', (req, res) => {
     delete req.session.user_uid;
     res.redirect('/main');
@@ -186,6 +135,7 @@ app.get('/master_main/master_study/add', function(req, res) {
         res.render('add', {boards: board01});
     });
 });
+
 app.post('/master_main/master_study/add', function (req, res) {
     var Title = req.body.Title;
     var Content = req.body.Content;
@@ -196,7 +146,7 @@ app.post('/master_main/master_study/add', function (req, res) {
             console.log(err);
             res.status(500).send('Server Error');
         } else {
-            res.redirect('/main/study'+result.InsertId);
+            res.redirect('/master_main/master_study/'+result.InsertId);
         }
     })
 })
@@ -229,4 +179,3 @@ module.exports = app;
 app.listen(3000, function(){
     console.log('3000번 포트에 연결되었습니다!');
 });
-
