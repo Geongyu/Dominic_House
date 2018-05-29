46871c1b505bafeae14ee5aab3cbcec3f739ed4e
SuhangGeongyu <37532168+SuhangGeongyu@users.noreply.github.com>
2018-05-29 15:08:51 +0900

    Add files via upload

--- 시작을 위한 한걸음/app.js
+++ 시작을 위한 한걸음/app.js
@@ -3,7 +3,12 @@ var bodyParser = require('body-parser');
 var mysql = require('mysql');
 var app = express();
 var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var passport = require('passport')
+    , LocalStrategy = require('passport-local').Strategy;
+var flash = require('connect-flash');
 
+// 로컬에서 실험하고자함
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -13,15 +18,8 @@ var connection = mysql.createConnection({
 connection.connect();
 // mysql 연동을 위한
 
-var passport = require('passport');
-// require('./config/passport')(passport);
-app.use(passport.initialize());
-app.use(passport.session());
-//로그인 세션 유지
-
-
 app.use(session({
-    secret: 'keyboard cat',
+    secret: 'atc54321',
     resave: false,
     saveUninitialized: true
 }));
@@ -32,6 +30,45 @@ app.set('view engine', 'jade');
 app.set('views', './views');
 app.use(bodyParser.urlencoded({ extended: false}));
 // catch 404 and forward to error handler
+app.use(cookieParser());
+app.use(flash());
+//app.use(bodyParser.json);
+
+app.use(passport.initialize());
+app.use(passport.session());
+// 미들웨어 설정
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+passport.deserializeUser(function (id, done) {
+    // DB에서 ID를 이용하여 User를 얻어 Doen 호출
+    connection.query('SELECT * FROM mastertable WHERE `id`=?', [id], function (err, rows) {
+        var user = rows[0];
+        done(err, user);
+    });
+});
+
+passport.use(new LocalStrategy({
+    usernameField : 'username',
+    passwordField : 'password'
+}, function (username, password, done) {
+    connection.query('SELECT * FROM mastertable WHERE `username`=?', [username], function (err, rows) {
+        var user = rows[0];
+        if(err) {
+            return done(err);
+        }
+        if (!user) {
+            return done(null, false, { message : '아이디가 다릅니다'});
+        }
+        if (user.password !== password) {
+            return done(null, false, { message : '비밀번호가 다릅니다.'});
+        } else {
+            res.render('master_main');
+        }
+        return done(null, user);
+    });
+}));
 
 app.get(['/main', '/'], function (req, res) {
     res.render('main');
@@ -49,22 +86,7 @@ app.get('/main/develope', function (req, res) {
             res.render('develope', {boards: boards});
          })
 });
-
-const users = [
-    {
-        user_id: 'root',
-        user_pwd: '123456'
-    }
-];
-const findUser = (user_id, user_pwd) => {
-    // id와 password가 일치하는 유저 찾는 함수, 없으면 undefined 반환
-    return users.find( v => (v.user_id === user_id && v.user_pwd === user_pwd) );
-};
-const findUserIndex = (user_id, user_pwd) => {
-    // 일치하는 유저의 index값(유니크) 반환
-    return users.findIndex( v => (v.user_id === user_id && v.user_pwd === user_pwd) );
-};
-
+/*
 app.get(('/login'), function(req, res){
     res.render('login')
 });
@@ -77,6 +99,29 @@ app.post('/login', (req, res) => {
         res.render('master_main');
     } else {
         res.send('<script type="text/javascript">alert("올바르지 않은 사용자 입니다 사용자의 정보를 서버로 전송합니다.");</script>');
+    }
+});
+*/
+app.get('/master_main', function(req, res) {
+    if(req.user) {
+        res.render('master_main')
+    } else {
+        res.send('<script type="text/javascript">alert("올바르지 않은 사용자 입니다 사용자의 정보를 서버로 전송합니다.");</script>');
+    }
+});
+
+app.post('/login', passport.authenticate('local',{
+    successRedirect: '/master_main',
+    failureRedirect: '/login',
+    failureFlash: true
+}));
+
+app.get('/login', function (req, res) {
+    console.log(req.flash('error'));
+    if (req.user) {
+        res.send('이미 관리자 권한으로 로그인 되었습니다.');
+    } else {
+        res.render('login');
     }
 });
 
